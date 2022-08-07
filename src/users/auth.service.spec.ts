96a5f7bff3c5b70560ac2f82478bb936624b649e
BrunaSepulveda <sepulvedabruna@gmail.com>
2022-08-07 17:28:47 -0300

    ajuste e criação de testes

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,21 +1,58 @@
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { Users } from './users.entity';
 import { UsersService } from './users.service';
 
-it('cria uma instancia do authservice', async () => {
-  // cria um user service falso
-  const fakeUsersService = {
-    find: () => Promise.resolve([]),
-    create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password }),
-  }
+describe('AuthService', () => {
+  let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
 
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthService,
-      { provide: UsersService, useValue: fakeUsersService },
-    ],
-  }).compile();
-  const service = module.get(AuthService);
-  expect(service).toBeDefined();
+  beforeEach(async () => {
+    fakeUsersService = {
+      findEmail: () => Promise.resolve(null),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password } as Users),
+    };
+
+    const module = await Test.createTestingModule({
+      providers: [
+        // precisamos colocar todas as dependencias e as classes que vamos testar
+        AuthService,
+        // AuthService, tem a dependencia do UsersService,
+        // então podemos criar um objeto com funções para ser o valor
+        // Logo qualquer clase dentro desse módulo que precisar
+        // da depencia de UsersService usará o objeto criado
+        { provide: UsersService, useValue: fakeUsersService },
+      ],
+    }).compile();
+    service = module.get(AuthService);
+  });
+
+  it('cria uma instancia do authservice', async () => {
+    expect(service).toBeDefined();
+  });
+
+  it('creates a new user with a salted and hashed password', async () => {
+    const user = await service.signup('bruna@gmail.com', 'blabla22');
+
+    expect(user.password).not.toEqual('blabla22');
+
+    const [salt, hash] = user.password.split('.');
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+  });
+
+  it('throws an error if user signs up with emal thats is in use', async () => {
+    fakeUsersService.findEmail = () =>
+      Promise.resolve({ id: 1, email: 'asdf@sss.com', password: '1' } as Users);
+    await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(
+      BadRequestException,
+    );
+  });
+  it('user not found', async () => {
+    await expect(service.signin('asdf@asdf.com', 'asdf')).rejects.toThrow(
+      NotFoundException,
+    );
+  });
 });
